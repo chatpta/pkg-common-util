ea8f4c8ab8f0ea567b10b02796d04aa4034a2905
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2022-01-08 00:40:51 -0500

    tests added

--- test/appErrorText.js
+++ test/appErrorText.js
@@ -49,4 +49,148 @@ describe( "Application error handler test", function () {
                 done();
             } );
     } );
+
+    it( "throwWrongCredentialsError", function ( done ) {
+
+        // Act
+        request( testApp( errHandler.throwWrongCredentialsError ) )
+            .get( '/' )
+            .expect( 200 )
+            .end( ( err, response ) => {
+                if ( err ) return;
+
+                // Assert
+                assert( response.status === 200 );
+                assert.deepStrictEqual( response?.body?.error, "wrong_credentials" );
+                done();
+            } );
+    } );
+
+    it( "throwLoginRequiredError", function ( done ) {
+
+        // Act
+        request( testApp( errHandler.throwLoginRequiredError ) )
+            .get( '/' )
+            .expect( 200 )
+            .end( ( err, response ) => {
+                if ( err ) return;
+
+                // Assert
+                assert( response.status === 200 );
+                assert.deepStrictEqual( response?.body?.error, "login_required" );
+                done();
+            } );
+    } );
+
+    it( "throwRecordNotFoundError", function ( done ) {
+
+        // Act
+        request( testApp( errHandler.throwRecordNotFoundError ) )
+            .get( '/' )
+            .expect( 200 )
+            .end( ( err, response ) => {
+                if ( err ) return;
+
+                // Assert
+                assert( response.status === 200 );
+                assert.deepStrictEqual( response?.body?.error, "record_not_found" );
+                done();
+            } );
+    } );
+
+    it( "throwRecordNotSavedError", function ( done ) {
+
+        // Act
+        request( testApp( errHandler.throwRecordNotSavedError ) )
+            .get( '/' )
+            .expect( 200 )
+            .end( ( err, response ) => {
+                if ( err ) return;
+
+                // Assert
+                assert( response.status === 200 );
+                assert.deepStrictEqual( response?.body?.error, "record_save_failure" );
+                done();
+            } );
+    } );
+
+    it( "throwUpdateFailedError", function ( done ) {
+
+        // Act
+        request( testApp( errHandler.throwUpdateFailedError ) )
+            .get( '/' )
+            .expect( 200 )
+            .end( ( err, response ) => {
+                if ( err ) return;
+
+                // Assert
+                assert( response.status === 200 );
+                assert.deepStrictEqual( response?.body?.error, "update_failed" );
+                done();
+            } );
+    } );
+
+    it( "throwTransactionFailedError", function ( done ) {
+
+        // Act
+        request( testApp( errHandler.throwTransactionFailedError ) )
+            .get( '/' )
+            .expect( 200 )
+            .end( ( err, response ) => {
+                if ( err ) return;
+
+                // Assert
+                assert( response.status === 200 );
+                assert.deepStrictEqual( response?.body?.error, "transaction_failed" );
+                done();
+            } );
+    } );
+
+    it( "throwUsedTokenError", function ( done ) {
+
+        // Act
+        request( testApp( errHandler.throwUsedTokenError ) )
+            .get( '/' )
+            .expect( 200 )
+            .end( ( err, response ) => {
+                if ( err ) return;
+
+                // Assert
+                assert( response.status === 200 );
+                assert.deepStrictEqual( response?.body?.error, "wrong_credentials" );
+                done();
+            } );
+    } );
+
+    it( "throwFileFormatNotSupportedError", function ( done ) {
+
+        // Act
+        request( testApp( errHandler.throwFileFormatNotSupportedError ) )
+            .get( '/' )
+            .expect( 200 )
+            .end( ( err, response ) => {
+                if ( err ) return;
+
+                // Assert
+                assert( response.status === 200 );
+                assert.deepStrictEqual( response?.body?.error, "file_format_not_supported" );
+                done();
+            } );
+    } );
+
+    it( "throwNotAuthorizedError", function ( done ) {
+
+        // Act
+        request( testApp( errHandler.throwNotAuthorizedError ) )
+            .get( '/' )
+            .expect( 200 )
+            .end( ( err, response ) => {
+                if ( err ) return;
+
+                // Assert
+                assert( response.status === 200 );
+                assert.deepStrictEqual( response?.body?.error, "not_authorized" );
+                done();
+            } );
+    } );
 } );
